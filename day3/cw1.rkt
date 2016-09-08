4b296f2b07dbf21caa252eb8f15424e02852d0df
joeylmaalouf <joeylmaalouf@gmail.com>
2016-09-08 15:14:46 -0400

    classwork

---
+++ day3/cw1.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(define (tree-add1 tree)
+  (cond
+    [(null? tree) tree]
+    [(number? tree) (+ tree 1)]
+    [(null? (rest tree)) (tree-add1 (first tree))]
+    [else (list (tree-add1 (first tree)) (tree-add1 (rest tree)))]
+  )
+)
+(display (tree-add1 '(3 (1 4)))) (newline)
