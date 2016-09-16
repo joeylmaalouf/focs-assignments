0a6a618e5df89cd766e49a2746e4dcfbcabf5ded
joeylmaalouf <joeylmaalouf@gmail.com>
2016-09-16 14:37:52 -0400

    made evaluate nice and concise, started interpreter

--- day5/hw4.rkt
+++ day5/hw4.rkt
@@ -1,8 +1,63 @@
 #lang racket
 
-;;; Student Name: Frankly Olin [change to your name]
+;;; Student Name: Joey Maalouf
 ;;;
 ;;; Check one:
-;;; [ ] I completed this assignment without assistance or external resources.
+;;; [X] I completed this assignment without assistance or external resources.
 ;;; [ ] I completed this assignment with assistance from ___
 ;;;     and/or using these external resources: ___
+
+(define operator-list
+  (list
+    (list 'ADD  +                           )
+    (list 'SUB  -                           )
+    (list 'MUL  *                           )
+    (list 'DIV  /                           )
+    (list 'GT   >                           )
+    (list 'LT   <                           )
+    (list 'GE   >=                          )
+    (list 'LE   <=                          )
+    (list 'EQ   =                           )
+    (list 'NEQ  (lambda (x y) (not (= x y))))
+    (list 'ANND (lambda (x y) (and x y    )))
+    (list 'ORR  (lambda (x y) (or  x y    )))
+    (list 'NOTT not                         )
+    (list 'IPH  (lambda (x y z) (if x y z) ))
+  )
+)
+
+(define variable-list
+  (list
+    (list 'x  3 )
+    (list 'y  2 )
+    (list 'z  -5)
+  )
+)
+
+(define (run)
+  (display "Welcome to my repl.") (newline)
+  (display "Type some scheme-ish at the prompt.") (newline)
+  (display "Type <return> after each expression:") (newline)
+  (repl)
+)
+
+(define (repl)
+  (display "> ")
+  (display (evaluate (read)))
+  (newline)
+  (repl)
+)
+
+(define (evaluate expr)
+  (cond
+    [(number? expr) expr]
+    [(boolean? expr) expr]
+    [(symbol? expr) (cadr (assq expr variable-list))]
+    [else (apply
+      (cadr (assq (car expr) operator-list))
+      (map evaluate (cdr expr))
+    )]
+  )
+)
+
+(run)
